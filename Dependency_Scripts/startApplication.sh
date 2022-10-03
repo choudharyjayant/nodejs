8611ab16115dfc35b5f197c53d4f183d81d781bd
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-10-03 11:40:28 +0530

    Update startApplication.sh

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-# Stop all servers and start the server as a daemon
-#forever stopall
-nohup node /home/ubuntu/nodejs/index.js > /dev/null 2>&1 &
+cd /home/ubuntu/nodejs/
+nohup node index.js > /dev/null 2>&1 &
