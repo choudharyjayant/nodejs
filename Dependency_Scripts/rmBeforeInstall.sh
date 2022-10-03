303889effffa63a25075e303f71ab3fba97a09a2
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-10-03 11:39:46 +0530

    Update rmBeforeInstall.sh

--- Dependency_Scripts/rmBeforeInstall.sh
+++ Dependency_Scripts/rmBeforeInstall.sh
@@ -1,2 +1,4 @@
-#! /bin/bash
-sudo rm -rf  /var/lib/jenkins/workspace/jenkins-nodejs/*
+#!/bin/bash
+cd /home/ubuntu/nodejs/
+for pid in $(lsof -t -i:3000); do                        kill -9 $pid;                done
+sudo rm -rf *
